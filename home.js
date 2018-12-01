7bb8f6cb54ba24ec00a47bbe7bf8df46c6e0240a
rouegrace <roue@me.com>
2018-12-01 16:23:16 +0800

    day 5

---
+++ home.js
@@ -0,0 +1,40 @@
+var my_name;
+var my_age;
+var my_birthday;
+var my_test;
+
+my_name = "Stackup";
+my_age = 23;
+my_birthday = "20/03/1988";
+
+var my_details = my_name + my_age;
+var firstnum = 5;
+var secnum = 2;
+var total = firstnum + secnum;
+
+console.log(my_name);
+console.log(my_age);
+console.log(my_birthday);
+console.log(my_details);
+console.log(total);
+console.log(typeof(my_test));
+console.log(typeof(my_name));
+console.log(typeof(my_age));
+
+
+var firstname = "nick";
+var lastname = "carter";
+var space = " ";
+
+var fullname = firstname + space + lastname;
+console.log(fullname);
+
+var fullname2 = firstname + 5 + lastname;
+console.log(fullname2);
+
+var qty = 2;
+var total = 200;
+var qtyTotal = qty + total;
+console.log(qtyTotal);
+
+
